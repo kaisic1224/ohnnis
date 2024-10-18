d3cada26ed6d8503f67664656dfe92a350446225
kaisic1224 <hiddenshadows1224@gmail.com>
2024-10-17 20:50:18 -0400

    feat: test fingerprint fn

--- libs/proto/src/main.rs
+++ libs/proto/src/main.rs
@@ -1,13 +1,153 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use core::fmt;
+use std::net::{SocketAddr, ToSocketAddrs};
+
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1224").await.unwrap();
 
-    loop {
-        let (socket ) = listener.accept();
+    fingerprint("fuckvoidopps.com/spoiler-alert/", UserAgent::FfLinux, Locale::enUS).await;
+    // let listener = TcpListener::bind("127.0.0.1:1224").await.unwrap();
+    //
+    // loop {
+    //     let (mut socket, _) = listener.accept().await.unwrap();
+    //     tokio::spawn(async move {
+    //        println!("connection opened");
+    //         socket.write(b"something");
+    //     });
+    // }
+}
+
+
+
+
+enum UserAgent {
+    FfMacOs,
+    ChromeMacOs,
+    ChromeWin10,
+    FfLinux
+}
+
+impl UserAgent {
+    fn value(&self) -> &str {
+        match *self {
+            UserAgent::FfMacOs => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0",
+            UserAgent::ChromeMacOs => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+            UserAgent::ChromeWin10 => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+            UserAgent::FfLinux => "Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0"
+        }
     }
+}
+
+impl fmt::Display for UserAgent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+enum Locale {
+    enUS,
+    koKR
+}
+
+impl Locale {
+    fn value(&self) -> &str {
+        match *self {
+            Locale::enUS => "en-US",
+            Locale::koKR => "ko-KR"
+        }
+    }
+}
+
+impl fmt::Display for Locale {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+fn get_base_url(url: &str) -> (&str, String) {
+    let paths = url.split("/").collect::<Vec<&str>>();
+
+    // remove any "https:// or http:// from beginning"
+
+    (paths[0], paths[1..].join("/").to_string())
+}
+
+// ==== fingerprinting the server ====
+// fingerprint(url: &str) takes in a url and performs a request
+// to the url, then returns a HTTPHeader denoting the fingerprint
+// of the server.
+// assumes: url is in a format with only the TLD and SLD
+async fn fingerprint(url: &str, user_agent: UserAgent, locale: Locale) {
+    // first step, change the url from human-readable into its ip using dns-lookup
+    // no syscall, we use our custom function
+    let (base, path) = get_base_url(url);
+    let mut ip: String = base.to_owned();
+    ip.push_str(":80");
+    // other common ports include 21 (FTP), 587 (SMTP)
+    let addr = ip.to_socket_addrs().unwrap().collect::<Vec<SocketAddr>>()[0];
+    let mut conn = TcpStream::connect(addr).await.unwrap();
+
+    let path = format!("GET /{} HTTP/1.1", path);
+    let host = format!("Host: {base}");
+    let ua = format!("User-Agent: {}", user_agent.value());
+    let lang = format!("Accept-Language: {},{};q=0.5", locale.value(), locale.value().split("-").collect::<Vec<&str>>()[0]);
+    let resp = [path.as_str(),
+                host.as_str(),
+                ua.as_str(),
+                "Accept: text/html, application/xhtml_xml, application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                lang.as_str(),
+                "Accept-Encoding: gzip, deflate, br",
+                // "DNT: 1",
+                // "Connection: keep-alive",
+                // "Upgrade-Insecure-Requests: 1",
+                // "Sec-Fetch-Dest: document",
+                // "Sec-Fetch-Mode: navigate",
+                // "Sec-Fetch-Site: none",
+                // "Sec-Fetch-User: ?1",
+                "Cache-Control: max-age=0",
+                "\r\n"
+    ].join("\r\n");
+
+    println!("{resp}");
+    conn.write_all(resp.as_bytes()).await.unwrap();
+    conn.flush().await.unwrap();
+
+    let buf_reader = BufReader::new(&mut conn);
+    let mut lines = buf_reader.lines();
+    let mut headers: Vec<String> = vec![];
+    while let Some(line) = lines.next_line().await.unwrap() {
+        // println!("length = {}", line.len());
+        println!("Request: {line:#?}");
+        headers.push(line);
+    }
+    // loop {
+    //     let mut line = String::new();
+    //     buf_reader.read_line(&mut line).await.unwrap();
+    //     buf_reader.flush().await.unwrap();
+    //     if line.is_empty() {
+    //         break;
+    //     }
+    //
+    //     println!("{line:#?}");
+    // }
+
+
+    // let res = String::from_utf8(buf.to_vec()).unwrap();
+    // println!("{res}");
+
+    conn.shutdown().await.unwrap();
+    // let (stream, _) = listener.accept().await.unwrap();
+    //
+    // let buf_reader = BufReader::new(stream.into_std().unwrap());
+    // let req: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    //
+    // println!("{req:#?}")
 }
 
 
